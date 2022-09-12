59a9f0c2097c0e7e92262d0d2e0d4d0f1f809bac
BrandonHalmai <76132565+BrandonHalmai@users.noreply.github.com>
2022-09-13 02:03:56 +1000

    Add files via upload

--- EchoThread.java
+++ EchoThread.java
@@ -1,10 +1,26 @@
-package application;
+package application; //This refers to the folder that is currently holding the class; this is so classes can access our
+//public methods, i.e. our main method.
+
+//These are imported libraries that are not built into java, so we import them to utilise their functionalities.
 
 import java.io.*;
 import java.net.Socket;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
+/**
+Here we're creating a class called EchoThread that utilises the existing library 'Thread'. This will be used so that
+the server can perform many tasks more efficiently and effectively over multiple threads. We first create an object
+called socket from the Socket library and assign that to the EchoThread class by equating it to the variable
+'clientsocket'. After this we create our 'run' method which will gather the data from the client and write it to a
+file. This is done by creating the object 'InputStreamReader' from the Thread library that reads the stream of data
+coming from the client. It's then supported by the 'BufferedReader' object that will receive that input and
+output the contents into a variable. We then define a variable called 'path' that will store the invoice's file
+location; this will be /$USER/home/Invoices/ where $USER is the user currently executing the application. If this does
+exist, then we simply create a folder in the home directory. The last part handles the file saves; we use the
+'BufferedWriter' object to save the file to the location defined in the 'path' variable along with the time. We then
+finish by stating the file has been saved.
+ */
 public class EchoThread extends Thread {
     protected Socket socket;
 
--- Server.java
+++ Server.java
@@ -1,17 +1,31 @@
-package application;
+package application; //This refers to the folder that is currently holding the class; this is so classes can access our
+//public methods, i.e. our main method.
 
+//These are imported libraries that are not built into java, so we import them to utilise their functionalities.
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+Here is our main method; it is set to public, so it can be accessed by the other classes in our program. Along with
+static and void, so we don't need an object to use it and return a value.
+*/
 public class Server {
     public static void main(String args[]) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(6789);
-        System.out.println("Listening to port 6789");
+        ServerSocket serverSocket = new ServerSocket(6789); //Here we are creating a new object called
+        //'serverSocket' from the library ServerSocket. This will allow us to establish, maintain and terminate a
+        //connection. We've also inputted an integer value of 6789 which will be port the connection is running on.
+        System.out.println("Listening to port 6789"); //Here we are printing out what port the server is running on.
 
-        new MenuThread().start();
+        new MenuThread().start(); //This will be used to create a new object from MenuThread, create a new thread and
+        //run it. This will make the program run more smoothly and efficiently because it's utilising multiple threads.
 
+        /**
+        We've included a while loop that starts a new thread for each new connection. This is done by accepting the
+        new connection and using a random port to communicate to the clients. Finally, we print our client's IP
+        address as well as its port.
+         */
         while(true) {
             Socket socket = serverSocket.accept();
             new EchoThread(socket).start();
